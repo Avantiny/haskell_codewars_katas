d741c95eb24ea217f973b086e2996da79d0f2d6e
dominik.kure <dominik.kure@kiwi.com>
2022-11-15 17:56:54 +0100

    test: snail tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import SixKyu
+import SixKyu ( findNb )
+import FourKyu ( snail )
 import Kata
 import Test.Hspec
 
@@ -12,3 +13,18 @@ main = hspec $ do
       findNb 36 `shouldBe` 3
     it "dominik" $ do
       dominik `shouldBe` 1
+    it "Snail - first example" $ do
+      let array = [[1,2,3],
+                  [4,5,6],
+                  [7,8,9]]
+      let expected = [1,2,3,6,9,8,7,4,5]
+      snail array `shouldBe` expected
+    it "Snail - Second example" $ do
+      let array = [[1,2,3],
+                  [8,9,4],
+                  [7,6,5]]
+      let expected = [1,2,3,4,5,6,7,8,9]
+      snail array `shouldBe` expected
+
+    
+      
